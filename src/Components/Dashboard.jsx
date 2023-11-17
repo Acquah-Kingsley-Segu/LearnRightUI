a7ae269232f03f0100a62919732061cee57d1981
Kingsley Segu Acquah <kingsley.acquah@amalitech.com>
2023-11-17 10:18:31 +0000

    feat: toast a successful login message

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import DashboardNavs from "./SubComponent/DashboardNavs";
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const Dashboard = () => {
+  const location = useLocation();
+  useEffect(() => {
+    toast.success(location["state"]["message"]);
+  }, [location.pathname]);
+
   return (
     <div className="flex h-screen">
       <DashboardNavs />
       <main className="flex-1 border m-1 rounded-lg mr-2">
         <Outlet />
+        <ToastContainer />
       </main>
     </div>
   );
